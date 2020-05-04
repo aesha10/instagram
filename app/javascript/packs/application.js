604a9c280f574bf8cf3304e6e086d23e3748f18b
aesha10 <aeshagabha@gmail.com>
2020-05-04 10:03:02 -0700

    Styling of Index page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,19 +10,7 @@ require("@rails/ujs").start()
 //= require("turbolinks").start()
 //= require("@rails/activestorage").start()
 //= require("channels")
-
-$(function(){
-    $('#masonry-container').masonry({
-        itemSelector: '.box',
-        columnWidth: 200,
-        isAnimated: !Modernizr.csstransitions,
-        isFitWidth: true
-    });
-
-});
-
-
-
+//= require pic
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
